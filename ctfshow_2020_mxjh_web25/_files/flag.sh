690232f062f6420bdadf1371c8d6b50905285b55
GITEE <gitee@wujiaxing.cn>
2020-03-20 11:12:07 +0800

    update

--- ctfshow_2020_mxjh_web25/_files/flag.sh
+++ ctfshow_2020_mxjh_web25/_files/flag.sh
@@ -8,13 +8,15 @@ if [[ -z $FLAG_TABLE ]]; then
 	FLAG_TABLE="f1a9_i5_h3r3"
 fi
 
+mysql -e "USE ctfshow_web; \
+	ALTER TABLE FLAG_TABLE CHANGE FLAG_COLUMN $FLAG_COLUMN varchar(128) NOT NULL DEFAULT 'not_flag'; \
+	ALTER TABLE FLAG_TABLE RENAME $FLAG_TABLE; \
+	INSERT $FLAG_TABLE VALUES('${FLAG:0:14}');" -uroot -pctfshow
+
 echo ${FLAG:14:14} > /flag
 
 export FLAG=${FLAG:28:14}
 unset FLAG_TABLE
 unset FLAG_COLUMN
-
-# 修改数据库中的 FLAG
-mysql -e "USE ctfshow_web;ALTER TABLE FLAG_TABLE CHANGE FLAG_COLUMN $FLAG_COLUMN CHAR(128) NOT NULL DEFAULT 'not_flag';ALTER TABLE FLAG_TABLE RENAME $FLAG_TABLE;INSERT $FLAG_TABLE VALUES('${FLAG:0:14}');" -uroot -pctfshow
 
 rm -f /flag.sh
