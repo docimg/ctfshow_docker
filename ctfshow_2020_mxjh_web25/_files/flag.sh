7f80235aa6050a140ed00a6458fd6b0e02eef020
GITEE <gitee@wujiaxing.cn>
2020-03-22 14:51:06 +0800

    update

--- ctfshow_2020_mxjh_web25/_files/flag.sh
+++ ctfshow_2020_mxjh_web25/_files/flag.sh
@@ -13,6 +13,9 @@ mysql -uroot -pctfshow -e "USE ctfshow_web; \
 	ALTER TABLE FLAG_TABLE RENAME $FLAG_TABLE; \
 	INSERT INTO $FLAG_TABLE VALUES('${FLAG:0:21}');"
 
-echo ${FLAG:21:21} > /root/flag
+echo ${FLAG:21:21} > /flag
+
+chown root:root /flag -R
+chmod 700 /flag
 
 rm -rf /flag.sh
